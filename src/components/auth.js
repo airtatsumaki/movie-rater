5ffafd5f753e3d62aff003432ee28062c3f64a3e
tatsu <naz1mb20@gmail.com>
2021-09-19 10:24:24 +0100

    added cookie to index and auth

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 import { API } from '../api-service';
-import { TokenContext } from '../index';
+//import { TokenContext } from '../index';
+import { useCookies } from 'react-cookie';
 
 function Auth(){
   
@@ -10,18 +11,21 @@ function Auth(){
   
   //token and setToken are passed to this component via the 
   //TokenContext component witin index.js
-  const {token, setToken} = useContext(TokenContext);
+  //const {token, setToken} = useContext(TokenContext);
+  const [token, setToken] = useCookies(['token']);
 
   useEffect(() => {
+    // our token object has a propterty of token: '' inside.
+    // so have to check token['token']
     console.log(token);
-    if(token) {
+    if(token['token']) {
       window.location.href = "/movies";
     }
   },[token]);
 
   const loginClicked = () => {
     API.loginUser({username, password})
-    .then(resp => setToken(resp.token))
+    .then(resp => setToken('token', resp.token))
     //{username, password} - automatically replaced with {username: username, password: password}
     .catch(error => console.log(error));
     
@@ -29,15 +33,17 @@ function Auth(){
 
   return (
     <div className="App centerMe">
-      <div className="login">
-        <label htmlFor="username">Username</label><br />
-        <input id="username" type="text" placeholder="username" value={username} 
-        onChange={evt => setUsername(evt.target.value)} /><br />
-        <label htmlFor="password">Password</label><br />
-        <input id="password" type="password" placeholder="password" value={password} 
-        onChange={evt => setPassword(evt.target.value)} /><br />
-        <button onClick={loginClicked}>Login</button>
-      </div>
+      {!token['token'] ? (
+        <div className="login">
+          <label htmlFor="username">Username</label><br />
+          <input id="username" type="text" placeholder="username" value={username} 
+          onChange={evt => setUsername(evt.target.value)} /><br />
+          <label htmlFor="password">Password</label><br />
+          <input id="password" type="password" placeholder="password" value={password} 
+          onChange={evt => setPassword(evt.target.value)} /><br />
+          <button onClick={loginClicked}>Login</button>
+        </div>
+      ) : null}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
-import React, { useState, createContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Auth from './components/auth';
 import { BrowserRouter, Route } from "react-router-dom";
+import { CookiesProvider } from 'react-cookie';
 
 //context used as a global variable for all components
-export const TokenContext = createContext(null);
+//context replaced with cookies
+//export const TokenContext = createContext(null);
 
 function Router(){
 
   // this state is passed via a context so all children to use it
-  const [token, setToken] = useState('');
+  // when using cookies no longer need the state here as <CookiesProvider>
+  // does this for us
+  // const [token, setToken] = useState('');
 
   return (
     
     <React.StrictMode>
       {/* wrapping the app in the context tag lets all children 
           read the context data from value=TOKEN*/}
-      <TokenContext.Provider value={{token, setToken}}>
+      {/* <TokenContext.Provider value={{token, setToken}}> */}
+      <CookiesProvider>
         <BrowserRouter>
           <Route exact path="/" component={Auth} />
           <Route exact path="/movies" component={App} />
         </BrowserRouter>
-      </TokenContext.Provider>
+      {/* </TokenContext.Provider> */}
+      </CookiesProvider>
     </React.StrictMode>
   )
 }
