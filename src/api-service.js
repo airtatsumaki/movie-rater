cbf653bf18747a8951dd011b099e109375836096
tatsu <naz1mb20@gmail.com>
2021-09-22 16:15:19 +0100

    register user complete

--- src/api-service.js
+++ src/api-service.js
@@ -13,6 +13,18 @@ export class API{
     );
   }
 
+  static registerUser(body){
+    return (
+      fetch(`http://127.0.0.1:8000/api/users/`,{
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+      }).then(resp => resp.json())
+    );
+  }
+
   static updateMovie(movieID, body, token){
     return (
       fetch(`http://127.0.0.1:8000/api/movies/${movieID}/`,{
--- src/components/auth.js
+++ src/components/auth.js
@@ -8,7 +8,8 @@ function Auth(){
   
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  
+  const [isLoginView, setIsLoginView] = useState(true);
+
   //token and setToken are passed to this component via the 
   //TokenContext component witin index.js
   //const {token, setToken} = useContext(TokenContext);
@@ -31,17 +32,35 @@ function Auth(){
     
   }
 
+  const registerClicked = () => {
+    API.registerUser({username, password})
+    .then(() => loginClicked())
+    //{username, password} - automatically replaced with {username: username, password: password}
+    .catch(error => console.log(error));
+  }
+
   return (
     <div className="App centerMe">
       {!token['token'] ? (
         <div className="login">
+          {isLoginView ? (<h1>Login</h1>) : (<h1>Register</h1>) }
           <label htmlFor="username">Username</label><br />
           <input id="username" type="text" placeholder="username" value={username} 
           onChange={evt => setUsername(evt.target.value)} /><br />
           <label htmlFor="password">Password</label><br />
           <input id="password" type="password" placeholder="password" value={password} 
           onChange={evt => setPassword(evt.target.value)} /><br />
-          <button onClick={loginClicked}>Login</button>
+          {isLoginView ? (
+            <>
+            <button onClick={loginClicked}>Login</button>
+            <p>Don't have an account? Register <span className="link clickable" onClick={() => setIsLoginView(false)}>here!</span></p>
+            </>
+          ) : (
+            <>
+            <button onClick={registerClicked}>Register</button>
+            <p>Already have an account? Sign in <span className="link clickable" onClick={() => setIsLoginView(true)}>here!</span></p>
+            </>
+          ) }
         </div>
       ) : null}
     </div>
